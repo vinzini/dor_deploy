356061215d572668087b3815cd2934385a4553d6
vinay <vinay.p@ziniostech.com>
2020-05-11 13:23:42 +0530

    Initial Commit

---
+++ service/hr/tests/Unit/CheckTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CheckTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(false);
+    }
+}
